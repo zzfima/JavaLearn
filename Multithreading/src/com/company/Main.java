5bfeab34954d08f9db5b51358c132a145c704f9e
zzfima <zzfima@gmail.com>
2021-08-06 21:12:34 +0300

    rfct

--- Multithreading/src/com/company/Main.java
+++ Multithreading/src/com/company/Main.java
@@ -6,7 +6,9 @@ public class Main {
         //1 way
         Thread t1 = new Thread() {
             public void run() {
-                System.out.println("Thread Running1");
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Thread Running1");
+                }
             }
         };
         t1.start();
@@ -17,15 +19,23 @@ public class Main {
 
         //3 way
         Runnable runnable = () -> {
-            System.out.println("Thread Running3");
+            for (int i = 0; i < 10; i++)
+                System.out.println("Thread Running3");
         };
         Thread t3 = new Thread(runnable);
         t3.start();
+
+        //4 way
+        (new Thread(() -> {
+            for (int i = 0; i < 10; i++)
+                System.out.println("Thread Running4");
+        })).start();
     }
 }
 
 class MyThread extends Thread {
     public void run() {
-        System.out.println("Thread Running2");
+        for (int i = 0; i < 10; i++)
+            System.out.println("Thread Running2");
     }
 }
