0701dcb61009a08338d0ab0658759c2bf3aab583
zzfima <zzfima@gmail.com>
2021-08-06 17:12:14 +0300

    dd threads

--- Multithreading/src/com/company/Main.java
+++ Multithreading/src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Thread t1 = new Thread() {
+            public void run() {
+                System.out.println("Thread Running1");
+            }
+        };
+        t1.start();
+
+        MyThread t2 = new MyThread();
+        t2.start();
+
+        Runnable runnable = ()->{
+            System.out.println("Thread Running3");
+        };
+        Thread t3 = new Thread(runnable);
+        t3.start();
+    }
+}
+
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Thread Running2");
     }
 }
