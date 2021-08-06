204417b5e080219a6c2c849a0e9fad60d9f2a6a7
zzfima <zzfima@gmail.com>
2021-08-06 10:39:37 +0300

    print app

---
+++ Print/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        System.out.print("You entered " + i);
+    }
+}
