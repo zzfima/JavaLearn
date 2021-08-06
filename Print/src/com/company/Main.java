1d83fc555342a300a7b7374f9df2d76ef709f273
zzfima <zzfima@gmail.com>
2021-08-06 10:46:32 +0300

    rfct

--- Print/src/com/company/Main.java
+++ Print/src/com/company/Main.java
@@ -8,6 +8,22 @@ public class Main {
         System.out.println("Hello World");
         Scanner scanner = new Scanner(System.in);
         int i = scanner.nextInt();
-        System.out.print("You entered " + i);
+        System.out.println("You entered " + i);
+
+        switch (i) {
+            case 10 -> System.out.println("You entered 10");
+            case 15 -> System.out.println("You entered 15");
+            default -> System.out.println("You entered nor 10 nor 15");
+        }
+        /** OLD fashion switch
+        switch (i) {
+            case 10:
+                System.out.println("You entered 10");
+                break;
+            default:
+                System.out.println("You entered not 10");
+                break;
+        }
+        **/
     }
 }
