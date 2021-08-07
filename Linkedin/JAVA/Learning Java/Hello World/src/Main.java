614e7e9d145923bd7156562c1af72d3b9e4cdbaa
zzfima <zzfima@gmail.com>
2021-08-07 19:29:13 +0300

    rfct

--- Linkedin/JAVA/Learning Java/Hello World/src/Main.java
+++ Linkedin/JAVA/Learning Java/Hello World/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world CMD " + FFF.bbb);
+        String msg = "Hello world CMD ";
+        System.out.println(msg + FFF.bbb + " " + msg.charAt(2));
     }
 
     enum FFF {eee, bbb}
