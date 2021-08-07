a1d851a2d3009d319b8bc4457d2294aafcd2ba88
zzfima <zzfima@gmail.com>
2021-08-07 19:33:00 +0300

    rfct

--- Linkedin/JAVA/Learning Java/Hello World/src/Main.java
+++ Linkedin/JAVA/Learning Java/Hello World/src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String msg = "Hello world CMD ";
         System.out.println(msg + FFF.bbb + " " + msg.charAt(2));
+
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        System.out.println(i);
     }
 
     enum FFF {eee, bbb}
