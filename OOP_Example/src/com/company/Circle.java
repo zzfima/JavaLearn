32f4fefa6e119e5f824107112affa7a8c2ec3138
zzfima <zzfima@gmail.com>
2021-08-07 16:06:53 +0300

    move place

--- OOP_Example/src/com/company/Circle.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Circle implements IFigure {
-    @Override
-    public String GetName() {
-        return "I am CIrcle";
-    }
-}
--- OOP_Example/src/com/company/IFigure.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public interface IFigure {
-    String GetName();
-}
