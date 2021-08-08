ac67bb944ee96278e93e4ef2805e42c7d77161bc
zfima <zzfima@gmail.com>
2021-08-08 18:40:08 +0300

    start

---
+++ Linkedin/JAVA/Learning Java/Java 11 Essential Training/MemoryUsage/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] k = new int[10][];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(
+                    Runtime.getRuntime().maxMemory() + " " +
+                            Runtime.getRuntime().freeMemory() + " " +
+                            Runtime.getRuntime().totalMemory());
+
+            k[i] = new int[1_048_576];
+        }
+    }
+}
