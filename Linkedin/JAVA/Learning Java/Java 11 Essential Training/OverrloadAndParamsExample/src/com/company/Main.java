144d2b28d66a125d1070d2888427ad8582b8edcb
zfima <zzfima@gmail.com>
2021-08-09 15:11:37 +0300

    start

---
+++ Linkedin/JAVA/Learning Java/Java 11 Essential Training/OverrloadAndParamsExample/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sum(1, 2));
+        System.out.println(sum(1, 2, 3));
+        System.out.println(sum(1, 2, 3, 4));
+    }
+
+    private static int sum(int i1, int i2) {
+        return i1 + i2;
+    }
+
+    private static int sum(int i1, int i2, int i3) {
+        return i1 + i2 + i3;
+    }
+
+    private static int sum(int... numbers) {
+        int s = 0;
+        for (int i : numbers)
+            s += i;
+        return s;
+    }
+}
