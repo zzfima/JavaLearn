b66555ded4060f0b438c283c22122090a551872a
zfima <zzfima@gmail.com>
2021-08-09 10:42:49 +0300

    start

---
+++ Linkedin/JAVA/Learning Java/Java 11 Essential Training/NumberFormatExample/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NumberFormat numIntFormat = NumberFormat.getIntegerInstance();
+        System.out.println(numIntFormat.format(345.6));
+
+        NumberFormat numCurrFormatLocal = NumberFormat.getCurrencyInstance();
+        System.out.println(numCurrFormatLocal.format(345.6));
+
+        NumberFormat numCurrFormatUSA = NumberFormat.getCurrencyInstance(Locale.US);
+        System.out.println(numCurrFormatUSA.format(345.6));
+    }
+}
