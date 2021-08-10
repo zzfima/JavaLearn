308dd8f9592884de49eff9de481575ffdac04104
zfima <zzfima@gmail.com>
2021-08-10 08:24:53 +0300

    move

---
+++ Linkedin/JAVA/Learning Java/Java 11 Essential Training/ClassesExample/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import com.company.model.ClothingItem;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClothingItem clothingItem = new ClothingItem();
+        clothingItem.setQuantity(4);
+        System.out.println(clothingItem.getQuantity());
+
+        ClothingItem clothingItem1 = new ClothingItem(14, 44.3, "", "");
+        System.out.println(clothingItem1.getQuantity());
+
+        System.out.println(ClothingItem.MAX_QUANTITY);
+    }
+}
