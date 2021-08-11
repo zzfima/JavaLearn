df7e5f86ec6bbfb6533a47c1c59aeafdc822b19a
zfima <zzfima@gmail.com>
2021-08-11 10:30:33 +0300

    start

---
+++ Linkedin/JAVA/Learning Java/Learning Java Applications/ReuseJAR/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.codebind.TableData;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TableData tableData = new TableData();
+        System.out.println(tableData.getColumnCount());
+    }
+}
