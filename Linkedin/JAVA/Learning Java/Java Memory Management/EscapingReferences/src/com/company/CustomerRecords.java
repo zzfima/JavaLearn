073494204a49393b27a343abdac2c60113202443
zfima <zzfima@gmail.com>
2021-08-12 11:27:21 +0300

    start

---
+++ Linkedin/JAVA/Learning Java/Java Memory Management/EscapingReferences/src/com/company/CustomerRecords.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomerRecords {
+    private Map<String, Customer> records;
+
+    public CustomerRecords() {
+        this.records = new HashMap<String, Customer>();
+    }
+
+    public void addCustomer(Customer c) {
+        this.records.put(c.getName(), c);
+    }
+
+    public Map<String, Customer> getCustomers() {
+        return this.records;
+    }
+}
