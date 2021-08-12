fe0707e8bb731699a9b11afe96d55beb15fd154f
zfima <zzfima@gmail.com>
2021-08-12 09:13:44 +0300

    rfct

--- Linkedin/JAVA/Learning Java/Java Memory Management/MemoryExample/src/com/company/Main.java
+++ Linkedin/JAVA/Learning Java/Java Memory Management/MemoryExample/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Customer c = new Customer();
+        final Customer c = new Customer();
         System.out.println(c.getName());
         System.out.println(c.getName());
         System.out.println(c.getName());
