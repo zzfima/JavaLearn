3b04fc80280ca05bd08b2d7df2c139a24b5089cb
zfima <zzfima@gmail.com>
2021-08-12 09:27:32 +0300

    start

---
+++ Linkedin/JAVA/Learning Java/Java Memory Management/MemoryTest/src/com/company/Container.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class Container {
+    private String initial = "A";
+
+    public String getInitial() {
+        return initial;
+    }
+
+    public void setInitial(String initial) {
+        this.initial = initial;
+    }
+}
