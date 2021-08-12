70b3a1d30505aa4d53a068b15bfdcaa717e551c1
zfima <zzfima@gmail.com>
2021-08-12 09:28:21 +0300

    rfct

--- Linkedin/JAVA/Learning Java/Java Memory Management/MemoryTest/src/com/company/Main.java
+++ Linkedin/JAVA/Learning Java/Java Memory Management/MemoryTest/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Main main = new Main();
         main.start();
     }
