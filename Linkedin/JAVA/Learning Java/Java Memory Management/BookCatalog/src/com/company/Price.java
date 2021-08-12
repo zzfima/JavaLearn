1bf6256a1d1023f1ef96dd18ffbe12202324a8b2
zzfima <zzfima@gmail.com>
2021-08-12 17:35:47 +0300

    start

--- Linkedin/JAVA/Learning Java/Java Memory Management/BookCatalog/src/com/company/Price.java
+++ Linkedin/JAVA/Learning Java/Java Memory Management/BookCatalog/src/com/company/Price.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Price {
-    private Map<String, Double> rates;
+    private final Map<String, Double> rates;
     private Double value;
 
     public Price(Double value) {
