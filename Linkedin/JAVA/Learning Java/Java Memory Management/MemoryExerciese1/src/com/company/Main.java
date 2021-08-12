b30edae544609013405503c24bde671db6effddc
zfima <zzfima@gmail.com>
2021-08-12 09:23:14 +0300

    start

---
+++ Linkedin/JAVA/Learning Java/Java Memory Management/MemoryExerciese1/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final Customer c = new Customer();
+        System.out.println(c.getName());
+        System.out.println(c.getName());
+        System.out.println(c.getName());
+        System.out.println(c.getName());
+    }
+}
