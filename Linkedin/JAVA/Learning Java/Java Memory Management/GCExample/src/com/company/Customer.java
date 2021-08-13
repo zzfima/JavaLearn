231da912e451281f289b353ce5f0630aa7eee121
zzfima <zzfima@gmail.com>
2021-08-13 10:00:52 +0300

    start

---
+++ Linkedin/JAVA/Learning Java/Java Memory Management/GCExample/src/com/company/Customer.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Customer {
+    private final String name;
+
+    public Customer(String s) {
+        this.name = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Linkedin/JAVA/Learning Java/Java Memory Management/GCExample/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("Free memory before for: " + runtime.freeMemory() / (1024 * 1024) + " Mb");
+
+        for (int i = 0; i < 1000000; i++) {
+            Customer c = new Customer("Vasia" + i);
+        }
+
+        System.out.println("Free memory after for: " + runtime.freeMemory() / (1024 * 1024) + " Mb");
+
+        runtime.gc();
+
+        System.out.println("Free memory after gc: " + runtime.freeMemory() / (1024 * 1024) + " Mb");
+    }
+}
