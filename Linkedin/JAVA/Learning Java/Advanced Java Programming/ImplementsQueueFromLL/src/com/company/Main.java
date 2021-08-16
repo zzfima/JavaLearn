ea382cd5a160d11a1039403e36390e4d0e32f2a5
zfima <zzfima@gmail.com>
2021-08-16 12:56:50 +0300

    queueing all customers

--- Linkedin/JAVA/Learning Java/Advanced Java Programming/ImplementsQueueFromLL/src/com/company/Main.java
+++ Linkedin/JAVA/Learning Java/Advanced Java Programming/ImplementsQueueFromLL/src/com/company/Main.java
@@ -1,11 +1,28 @@
 package com.company;
 
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-        Customer customer1 = new Customer("Kolja");
-        System.out.println(customer1);
-        customer1.serve();
-        System.out.println(customer1);
+        Customer customer = new Customer("Kolja");
+        System.out.println(customer);
+        customer.serve();
+        System.out.println(customer);
+
+        LinkedList<Customer> customers = new LinkedList<>();
+        customers.add(new Customer("Molly"));
+        customers.add(new Customer("Harry"));
+        customers.add(new Customer("Alex"));
+        System.out.println(customers);
+        serveCustomers(customers);
+    }
+
+    private static void serveCustomers(LinkedList<Customer> customers) {
+        while (!customers.isEmpty()) {
+            Customer c = customers.remove(0);
+            c.serve();
+            System.out.println(c);
+        }
     }
 }
