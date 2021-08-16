d3a2150c17d4e68b9273dd6cbc55f586eb49dd03
zfima <zzfima@gmail.com>
2021-08-16 08:13:23 +0300

    rfct

--- Linkedin/JAVA/Learning Java/Learning JDBC/learning-jdbc/src/com/company/Main.java
+++ Linkedin/JAVA/Learning Java/Learning JDBC/learning-jdbc/src/com/company/Main.java
@@ -9,14 +9,21 @@ public class Main {
 
     public static void main(String[] args) {
         DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport", "postgres", "1511");
+        Connection connection = null;
         try {
-            Connection connection = dcm.getConnection();
+            connection = dcm.getConnection();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM customer");
             while (resultSet.next())
                 System.out.println(resultSet.getInt(1));
         } catch (SQLException e) {
             System.out.println("Error" + e.getMessage());
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
