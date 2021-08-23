c32947f4f512083e1c61e212c0a400c984cfb2f0
zzfima <zzfima@gmail.com>
2021-08-23 19:03:47 +0300

    rfct

--- Linkedin/JAVA/Learning Java/Java Persistence API JPA 1 The Basics/demo/demo/src/main/java/com/example/demo/entity/Ticket.java
+++ Linkedin/JAVA/Learning Java/Java Persistence API JPA 1 The Basics/demo/demo/src/main/java/com/example/demo/entity/Ticket.java
@@ -8,22 +8,19 @@ public class Ticket {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    private String title;
-    private String description;
-
     @ManyToOne
     @JoinColumn(name = "application_id")
     private Application application;
 
-    private String appId;
-
     @ManyToOne
-    @JoinTable(
-            name = "ticket_release",
+    @JoinTable(name = "ticket_release",
             joinColumns = @JoinColumn(name = "ticket_fk"),
             inverseJoinColumns = @JoinColumn(name = "release_fk"))
     private Release release;
 
+    private String appId;
+    private String title;
+    private String description;
     private String status;
 
     public Ticket() {
