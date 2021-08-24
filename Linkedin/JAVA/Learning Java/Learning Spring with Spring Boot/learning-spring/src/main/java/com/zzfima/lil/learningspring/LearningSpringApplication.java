6d4e5c2b6a1fe0a16aec072929792cbd0be75673
zfima <zzfima@gmail.com>
2021-08-24 16:16:38 +0300

    add rest handler rooms

--- Linkedin/JAVA/Learning Java/Learning Spring with Spring Boot/learning-spring/src/main/java/com/zzfima/lil/learningspring/LearningSpringApplication.java
+++ Linkedin/JAVA/Learning Java/Learning Spring with Spring Boot/learning-spring/src/main/java/com/zzfima/lil/learningspring/LearningSpringApplication.java
@@ -1,13 +1,32 @@
 package com.zzfima.lil.learningspring;
 
+import com.zzfima.lil.learningspring.data.entity.Room;
+import com.zzfima.lil.learningspring.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+
+	@RestController
+	@RequestMapping("/rooms")
+	public class RoomController{
+		@Autowired
+		private RoomRepository roomRepository;
+
+		@GetMapping
+		public Iterable<Room> getRooms(){
+			return this.roomRepository.findAll();
+		}
+
 	}
 
 }
