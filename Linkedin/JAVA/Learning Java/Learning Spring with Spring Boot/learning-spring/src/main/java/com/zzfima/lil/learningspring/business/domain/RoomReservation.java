2b67ecea00a6207c44890f594d9445dea02dac88
zzfima <zzfima@gmail.com>
2021-08-28 11:19:35 +0300

    rfct

---
+++ Linkedin/JAVA/Learning Java/Learning Spring with Spring Boot/learning-spring/src/main/java/com/zzfima/lil/learningspring/business/domain/RoomReservation.java
@@ -0,0 +1,69 @@
+package com.zzfima.lil.learningspring.business.domain;
+
+import java.time.LocalDate;
+
+public class RoomReservation {
+    private long roomId;
+    private long guestId;
+    private String roomName;
+    private String roomNumber;
+    private String firstName;
+    private String lastName;
+    private LocalDate resDate;
+
+    public long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
+    }
+
+    public long getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(long guestId) {
+        this.guestId = guestId;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getResDate() {
+        return resDate;
+    }
+
+    public void setResDate(LocalDate resDate) {
+        this.resDate = resDate;
+    }
+}
---
+++ Linkedin/JAVA/Learning Java/Learning Spring with Spring Boot/learning-spring/src/main/java/com/zzfima/lil/learningspring/business/service/ReservationService.java
@@ -0,0 +1,43 @@
+package com.zzfima.lil.learningspring.business.service;
+
+import com.zzfima.lil.learningspring.business.domain.RoomReservation;
+import com.zzfima.lil.learningspring.data.entity.Room;
+import com.zzfima.lil.learningspring.data.repository.GuestRepository;
+import com.zzfima.lil.learningspring.data.repository.ReservationRepository;
+import com.zzfima.lil.learningspring.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ReservationService {
+    private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
+
+    @Autowired
+    public ReservationService(RoomRepository roomRepository,
+                              GuestRepository guestRepository,
+                              ReservationRepository reservationRepository) {
+        this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
+    }
+
+    public List<RoomReservation> getRoomReservationForDate(LocalDate date) {
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        Map<Long, RoomReservation> roomReservationMap = new HashMap<>();
+        rooms.forEach(room -> {
+            RoomReservation roomReservation = new RoomReservation();
+            roomReservation.setRoomId(room.getRoomId());
+            roomReservation.setRoomName(room.getRoomName());
+            roomReservation.setRoomNumber(room.getRoomNumber());
+            roomReservationMap.put(room.getRoomId(), roomReservation);
+        });
+        return null;
+    }
+}
